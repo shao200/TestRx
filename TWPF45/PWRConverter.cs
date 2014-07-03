f920e7c27b58ab7d80aea4d69615551a456901a7
xyshyniaphy <xyshyniaphy@gmail.com>
2014-07-03 14:48:51 +0900

    double pwr converter ok
    Signed-off-by: xyshyniaphy <xyshyniaphy@gmail.com>

---
+++ TWPF45/PWRConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TWPF45
+{
+    [ValueConversion(typeof(double), typeof(String))]
+    public class PWRConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Format("{0}%", (int)((double)value * 100));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return 0;
+        }
+    }
+}
